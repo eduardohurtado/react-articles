d2edf079729765de7449dc49f9843623848c16de
Eduardo Hurtado <leduardohurtado@estudiante.uniajc.edu.co>
2020-10-20 23:06:40 -0500

    Version 1.2.4 - Trying parallax on AboutPage.

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Style
 import "./aboutPage.scss";
 
 const AboutPage: React.FC = () => {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    function handleScroll() {
+      setOffset(window.pageYOffset);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (offset) {
+      console.log(offset);
+    }
+  }, [offset]);
+
   return (
-    <div>
-      <h1>AboutPage.</h1>
+    <div className="aboutPage">
+      <div>
+        <h2
+          className="parallax"
+          style={{
+            backgroundPositionY: `translateY(${offset * 0.5}px)`,
+          }}
+        >
+          DIV 1
+        </h2>
+      </div>
+      <div>
+        <h2>DIV 2</h2>
+      </div>
+      <div>
+        <h2>DIV 3</h2>
+      </div>
+      <div>
+        <h2>DIV 4</h2>
+      </div>
     </div>
   );
 };
